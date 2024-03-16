8ffd13e3c41df4a16fbef83850c225fc5cfb3dbc
Mustafa Salim <salimmustafa763@gmail.com>
2024-03-16 04:07:17 +0300

    #3 zustand created store

--- src/components/pages/tweet/index.tsx
+++ src/components/pages/tweet/index.tsx
@@ -1,8 +1,10 @@
+import TweetForm from "./tweet-form"
 import TweetGenerator from "./tweet-generator"
 
 function Tweet() {
   return (
-    <section className="w-full h-dvh flex items-center justify-center">
+    <section className="w-full h-dvh flex items-center gap-x-40">
+      <TweetForm />
       <TweetGenerator />
     </section>
   )
--- src/store/tweet-slice/index.ts
+++ src/store/tweet-slice/index.ts
@@ -1,15 +1,57 @@
 import { create } from "zustand"
-import { combine } from "zustand/middleware"
 
 interface tweetOptions {
-  tweet: []
-  addDescription: (by: any) => void
+  username: any
+  fullname: string
+  avatar: any
+  descriptions: string
+  time: string
+  retweets: string
+  quoteTweets: string
+  likes: string
+
+  addUserName: (by: any) => void
 }
 
-const useTweetStore = create((set) => ({
-  tweet: [],
-  addDescription: (by: any) =>
-    set((state: any) => ({
-      tweet: [...state.tweet, by],
+export const useTweetStore = create<tweetOptions>()((set) => ({
+  username: "",
+  fullname: "",
+  avatar: "",
+  descriptions: "",
+  time: "",
+  retweets: "",
+  quoteTweets: "",
+  likes: "",
+  addUserName: (by: any) =>
+    set((state) => ({
+      username: (state.username = by),
+    })),
+  addFullName: (by: any) =>
+    set((state) => ({
+      fullname: (state.fullname = by),
+    })),
+  addAvatar: (by: any) =>
+    set((state) => ({
+      avatar: (state.avatar = by),
+    })),
+  addDescriptions: (by: any) =>
+    set((state) => ({
+      descriptions: (state.descriptions = by),
+    })),
+  addTime: (by: any) =>
+    set((state) => ({
+      time: (state.time = by),
+    })),
+  addRetweets: (by: any) =>
+    set((state) => ({
+      retweets: (state.retweets = by),
+    })),
+  addQuoteTweets: (by: any) =>
+    set((state) => ({
+      quoteTweets: (state.quoteTweets = by),
+    })),
+  addLikes: (by: any) =>
+    set((state) => ({
+      likes: (state.likes = by),
     })),
 }))
